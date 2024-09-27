d424a8cd6add9150b18588cbd984498c3ea7f8cc
Fefe <federicoluisestevez@gmail.com>
2024-09-27 15:54:51 -0300

    Added function structures

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 const buttonNum = document.getElementsByName("data-number");
 const buttonOp = document.getElementsByName("data-op");
-const buttonDel = document.getElementsByName("data-del");
-const buttonEq = document.getElementsByName("data-eq");
+const buttonDel = document.getElementsByName("data-del")[0];
+const buttonEq = document.getElementsByName("data-eq")[0];
 
 var result = document.getElementById("result");
+
+buttonNum.forEach(function(button){
+    button.addEventListener('click', function(){
+        addANumber(button.innerText);
+    })
+});
+
+buttonOp.forEach(function(button){
+    button.addEventListener('click', function(){
+        addAnOp(button.innerText);
+    })
+});
+
+buttonEq.addEventListener('click', function(){
+    calculate();
+    updateDisplay();
+});
+
+buttonDel.addEventListener('click', function(){
+    clear();
+    updateDisplay();
+});
